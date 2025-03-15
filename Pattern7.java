8ba69727024c86dbcbe95de04a10e4136ee03b9a
Alphamale916 <mathesh956639@gmail.com>
2025-03-15 20:52:25 +0530

    Create

---
+++ Pattern7.java
@@ -0,0 +1,29 @@
+/*Print a pyramid pattern of numbers.
+Explanation:
+Each row contains centered increasing numbers.
+Sample Input:
+•	4
+•	3
+Sample Output:
+•	   1
+  121
+ 12321
+1234321
+•	 1
+121
+12321
+ */
+import java.util.Scanner;
+public class Pattern7 {
+    public static void main(String[] alpha){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String space = " ";
+        for(int i=1;i<=n;i++){
+            System.out.print(space.repeat(n-i));
+            for(int j=1;j<=i;j++) System.out.print(j);
+            for(int j=i-1;j>0;j--) System.out.print(j);
+            System.out.println();
+        }
+    }
+}
